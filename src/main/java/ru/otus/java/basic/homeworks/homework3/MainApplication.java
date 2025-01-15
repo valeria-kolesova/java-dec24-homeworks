3c611b8bce86d10c8c2389650d26fed924ef87ba
Valeria Kolesova <valeria-it@yandex.ru>
2025-01-15 09:12:31 +0300

    greetings added

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/MainApplication.java
@@ -0,0 +1,19 @@
+package ru.otus.java.basic.homeworks.homework3;
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+        greetings();
+
+    }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+
+    }
+
+
+}
